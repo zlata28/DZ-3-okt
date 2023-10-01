80a04808bf61fad30a9d05f4b7e0099253ce6baa
zlata28 <larik288@yandex.ru>
2023-10-01 18:42:15 +0300

    function

--- java.js
+++ java.js
@@ -1,1 +1,34 @@
+//калькулятор-конструктор
+function Calculator(){
+    this.read = function(){
+      this.a = +prompt(0);
+      this.b = +prompt(0);
+    };
+    this.sum = function(){
+      return this.a + this.b;
+    };
+  
+    this.mul = function(){
+      return this.a * this.b;
+    };
+  }
+let calculator = new Calculator();
 
+alert("Sum=" + calculator.sum());
+
+alert("Mul=" + calculator.mul());
+
+//task2
+function Accumulator(startingValue){
+    this.value = startingValue;
+
+    this.read = function(){
+      this.value += +prompt('Сколько нужно добавить?');
+    };
+}
+let accumulator = new Accumulator(0); 
+
+accumulator.read(); 
+accumulator.read(); 
+
+alert(accumulator.value);
